1bb62d16f0009d24fba21b5dfb55f43e0d969d32
Anthony Randolph <acrand@us.ibm.com>
2023-06-14 10:56:30 -0700

    change env SEAA_EXTRAVARS -> SEAA_EXTRA_VARS

--- ibm/seaa/scripts/run_playbooks/run-undeploy-oel-dev-env.sh
+++ ibm/seaa/scripts/run_playbooks/run-undeploy-oel-dev-env.sh
@@ -50,7 +50,7 @@ function main() {
 
     #Run playbook
     ansible-playbook "${SEAA_CONFIG_PATH_TO_SE_ANSIBLE_ARTIFACTS}/playbooks/zos_dev_envs/oel/undeploy-oel-dev-env.yml" ${RUNOPTIONS} \
-     -e "${SEAA_EXTRAVARS}" --tags "${SEAA_TAGS}" --skip-tags "${SEAA_SKIPTAGS}" -e "${ev_automation_strategy:=}"
+     -e "${SEAA_EXTRA_VARS}" --tags "${SEAA_TAGS}" --skip-tags "${SEAA_SKIPTAGS}" -e "${ev_automation_strategy:=}"
 
     # Return Playbook exit code
     return $?
@@ -59,7 +59,7 @@ function main() {
 # Set defaults
 export SEAA_TAGS=
 export SEAA_SKIPTAGS=
-export SEAA_EXTRAVARS=
+export SEAA_EXTRA_VARS=
 export SEAA_INVENTORY="${SEAA_INVENTORY:-inventory.yaml}"
 export SEAA_INVENTORY_LOCATION="${SEAA_INVENTORY_LOCATION:-${SEAA_CONFIG_PATH_TO_SE_ANSIBLE_ARTIFACTS}/playbooks/inventory}"
 
