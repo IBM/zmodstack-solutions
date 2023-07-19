14ddc220e6f0b92f1ddda87bdd07511c10660842
Anthony Randolph <acrand@us.ibm.com>
2023-07-18 23:30:52 -0700

    add support for lookup with filter

--- ibm/seaa/ansible/plugins/validation/seaa_variables_validation.py
+++ ibm/seaa/ansible/plugins/validation/seaa_variables_validation.py
@@ -114,10 +114,14 @@ tmp_file_prefix = "vseaa"
 def bool_filter(value):
     return bool(value)
 
+def int_filter(value):
+    return int(value)
+
 def evaluate_jinja_template(template_string, variables):
     # Create a Jinja environment
     env = Environment()
     env.filters['bool'] = bool_filter
+    env.filters['int'] = int_filter
 
     template = env.from_string(template_string)
     rendered_value = template.render(variables)
@@ -147,16 +151,17 @@ def get_variable_value(module, value_string, variables):
     # Regex to match lookup variable string for values
     # lookup_match = re.match(r"{{\s*lookup\s*\(\s*(['\"]{1,2})(\w+)\1\s*,\s*(['\"]{1,2})(\w+)\3\s*\)\s*}}$", value_string)
     lookup_match = re.match(r"^\s*{{\s*lookup\s*\(\s*(['\"]{1,2})(\w+)\1\s*,\s*(['\"]{1,2})(\w+)\3\s*\)\s*}}$", value_string)
-
+    
     # Check if doing lookup for variable_sting
     if lookup_match:
-
+        
         # Get type of lookup supported lookups are ones are 'var and 'env'
         lookup_type = lookup_match.group(2)
 
         # Get variable name from "lookup"
         lookup_var_name = lookup_match.group(4)
 
+       
         if validate_jinja_template(value_string):
             # Do lookup for variables value string
             return _dolookup(module, lookup_type, lookup_var_name, variables)
@@ -174,11 +179,11 @@ def get_variable_value(module, value_string, variables):
             # Check if value is also a string could be a jinja template
             if isinstance(var_value, str):
                 # Check if value a jinja template or return value
-                return _check_and_return_value(var_value, variables)
+                return _check_and_return_value(module, var_value, variables)
             return var_value
         else:
             # Check if value a jinja template or return value
-            return _check_and_return_value(value_string, variables)
+            return _check_and_return_value(module, value_string, variables)
 
 # Method to do lookup (ansible vars or ENV) and jinja templating based on a specific variable and variables dictionary for specific host
 def _dolookup(module, lookup_type, lookup_var_name, variables):
@@ -200,11 +205,31 @@ def _dolookup(module, lookup_type, lookup_var_name, variables):
         raise ValidationException(f"Unsupported lookup type: {lookup_type}")
 
 
-def _check_and_return_value(value_string, variables):
+def _check_and_return_value(module, value_string, variables):
     jinja_value_match = re.match(r"({{.*?}})$", value_string)
     if jinja_value_match:
         value_jinja_str = jinja_value_match.group(1)
-    
+    # ^\s*
+    # \{\{\s*(lookup\([^)]+\))\s*\|\s*(\w+)\s*\}\}
+        # raise Exception(value_string)
+        
+        # lookup_w_filter_match = re.match(r"^\s*{{\s*(lookup\([^)]+\))\s*\|\s*(\w+)\s*\}\}", value_string)
+        lookup_w_filter_match = re.match(r"^\s*{{\s*lookup\('(\w+)',\s*'(\w+)'\)\s*\|\s*(\w+)\s*}}", value_string)
+        if lookup_w_filter_match:
+            # Get lookup_type
+            lookup_type = lookup_w_filter_match.group(1)
+
+            # Get lookup_var_name variable name
+            lookup_var_name = lookup_w_filter_match.group(2)
+            
+            # Get lookup_filter_name
+            lookup_filter_name = lookup_w_filter_match.group(3)
+
+            if lookup_filter_name != "bool" and lookup_filter_name != "int" :
+                raise ValidationException(f"Unsupported lookup filter {lookup_filter_name}")
+            else:
+                return _dolookup(module, lookup_type, lookup_var_name, variables)
+            
         value_string = evaluate_jinja_template(value_jinja_str, variables)
 
     return value_string
