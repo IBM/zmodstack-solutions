f42742b5603ce3ee7958cdf662b71a2995040d9a
Anthony Randolph <acrand@us.ibm.com>
2023-10-19 22:16:40 -0700

    add yaml_output dir env to run_playvook scripts

--- ibm/seaa/scripts/run_playbooks/run-deploy-oel-dev-env.sh
+++ ibm/seaa/scripts/run_playbooks/run-deploy-oel-dev-env.sh
@@ -49,7 +49,7 @@ function main() {
 
     #Run playbook
     ansible-playbook "${SEAA_CONFIG_PATH_TO_SE_ANSIBLE_ARTIFACTS}/playbooks/zos_dev_envs/oel/deploy-oel-dev-env.yml" ${RUNOPTIONS} \
-     -e "${SEAA_EXTRA_VARS}" -e "${ev_automation_strategy:=}"
+     -e "${SEAA_EXTRA_VARS}" -e "${ev_automation_strategy:=}" -e "${ev_yaml_output_dir:=}" 
 
     # Return Playbook exit code
     return $?
